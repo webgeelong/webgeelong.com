9d62639cd74a4c16915a08bb4755a42cc68bc8c7
Mir Misbah Ali <mirmisbahali@hotmail.com>
2025-10-17 01:58:52 +1100

    initial commit

---
+++ env.ts
@@ -0,0 +1,6 @@
+export const EnvVars = {
+  SITE_NAME: 'Web Services Geelong',
+  OG_IMAGES_URL: 'https://webgeelong.com/og-images/',
+  URL: 'https://webgeelong.com/',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://webgeelong.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
+};
